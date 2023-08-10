4826be5895bf715c2dd79b67f267941bf9544e0b
Mai <mai.zakaria.18@gmail.com>
2023-08-10 10:30:59 -0700

    first day of C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of char: %lu bytes\n", sizeof(char));
+printf("Size of short: %lu bytes\n", sizeof(short));
+printf("Size of int: %lu bytes\n", sizeof(int));
+printf("Size of long: %lu bytes\n", sizeof(long));
+printf("Size of long long: %lu bytes\n", sizeof(long long));
+printf("Size of float: %lu bytes\n", sizeof(float));
+return (0);
+}
