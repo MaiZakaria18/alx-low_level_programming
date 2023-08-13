b0646712c150d5d65a6e810dd8aa32c40e146c5d
Mai <mai.zakaria.18@gmail.com>
2023-08-13 09:04:13 -0700

    c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+int number = 0;
+while (number <= 9)
+{
+putchar(number + '0');
+number++;
+}
+putchar('\n');
+return (0);
+}
