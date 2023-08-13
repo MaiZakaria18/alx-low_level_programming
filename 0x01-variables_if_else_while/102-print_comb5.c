eb41e7bf95e5e281cb649b0d5532273684d814f5
Mai <mai.zakaria.18@gmail.com>
2023-08-13 14:06:30 -0700

    c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+int num1_tens, num1_ones, num2_tens, num2_ones;
+for (num1_tens = 0; num1_tens <= 9; num1_tens++)
+{
+for (num1_ones = 0; num1_ones <= 9; num1_ones++)
+{
+for (num2_tens = 0; num2_tens <= 9; num2_tens++)
+{
+for (num2_ones = 0; num2_ones <= 9; num2_ones++)
+{
+int num1 = num1_tens * 10 + num1_ones;
+int num2 = num2_tens * 10 + num2_ones;
+if (num1 < num2)
+{
+putchar(num1_tens + '0');
+putchar(num1_ones + '0');
+putchar(' ');
+putchar(num2_tens + '0');
+putchar(num2_ones + '0');
+if (num1 != 98 || num2 != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
