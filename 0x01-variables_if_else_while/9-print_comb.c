0a0898631c6ff191ba64f9b0dec381ae6659f0f0
Mai <mai.zakaria.18@gmail.com>
2023-08-13 13:14:10 -0700

    c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,15 +8,15 @@
 int main(void)
 {
 int number = 0;
-while (number <= 9)
+while (number < 10)
 {
 putchar(number + '0');
-number++;
 if (number != 9)
 {
 putchar(',');
 putchar(' ');
 }
+number++;
 }
 putchar('\n');
 return (0);
