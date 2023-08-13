be0499548593d199539ed4c996a94cb82db7b0dd
Mai <mai.zakaria.18@gmail.com>
2023-08-13 05:16:59 -0700

    C programing

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+char alphabet = 'a';
+while (alphabet <= 'z')
+{
+putchar(alphabet);
+alphabet++;
+}
+putchar('\n');
+return (0);
+}
