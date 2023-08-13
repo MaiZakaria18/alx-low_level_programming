c47504cd457490aea6dcbb74d9487821b5bc1354
Mai <mai.zakaria.18@gmail.com>
2023-08-13 13:33:20 -0700

    c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,14 +9,14 @@ int main(void)
 {
 int num1 = 0;
 int num2;
-while (num1 <= 9)
+while (num1 <= 8)
 {
-num2 = 0;
+num2 = num1 + 1
 while (num2 <= 9)
 {
 putchar(num1 + '0');
 putchar(num2 + '0');
-if (num1 != 9 || num2 != 9)
+if (num1 != 8 || num2 != 9)
 {
 putchar(',');
 putchar(' ');
