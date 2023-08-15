0c9497ff2096834f899f864a03d0d33dea859e88
Mai <mai.zakaria.18@gmail.com>
2023-08-15 04:42:46 -0700

    c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 #include "main.h"
 /**
- * 	main entery point
- *
- * Return: On success 0
- */
+* main entery point
+*
+* Return: On success 0
+*/
 int main(void)
 {
 printf("_putchar\n");
