3675d8077e2544cb87250276ce9b6778dbb60501
Mai <mai.zakaria.18@gmail.com>
2023-08-15 12:31:04 -0700

    c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - Entry point of the program
- *
- * Description:  prints the alphabet, in lowercase, followed by a new line.
- * 
- * Return: 0 on success
- */
+* main - Entry point of the program
+* Description:prints the alphabet, in lowercase
+* Return: 0 on success
+*/
 void print_alphabet(void)
 {
 int alphabet_lower = 'a';
