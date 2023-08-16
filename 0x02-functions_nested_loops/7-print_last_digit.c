06591cbaf13f4b02fb60a4b291f3a06403b31ae8
Mai <mai.zakaria.18@gmail.com>
2023-08-16 02:12:22 -0700

    c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,11 +11,11 @@ int print_last_digit(int number)
 int last_digit;
 if (number < 0)
 {
-last_digit = (n % 10) * -1;
+last_digit = (number % 10) * -1;
 }
 else
 {
-last_digit = (n % 10);
+last_digit = (number % 10);
 }
 _putchar(last_digit + 'o');
 return (last_digit);
