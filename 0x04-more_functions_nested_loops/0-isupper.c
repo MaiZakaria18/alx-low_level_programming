fe7f145e80f62b3368666eedb9614c3d51c60396
Mai <mai.zakaria.18@gmail.com>
2023-08-17 05:21:43 -0700

    c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  isupper - function that checks for uppercase character.
+ *
+ * Description - function that checks for uppercase character.
+ *
+ * @c: input for alphabet
+ *
+ * Return: 1 if c is uppercase Returns 0 otherwise
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
---
+++ 0x04-more_functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
+}
