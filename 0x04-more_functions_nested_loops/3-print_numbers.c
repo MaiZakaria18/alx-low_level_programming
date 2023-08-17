ae8574ce19a4ddd8179ad710967dc0c36b5b857c
Mai <mai.zakaria.18@gmail.com>
2023-08-17 06:52:04 -0700

    c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - function that prints the numbers, from 0 to 9
+ *
+ * Description - function that prints the numbers, from 0 to 9
+ *
+ * Return: 0 on success
+ */
+void print_numbers(void)
+{
+char number = 0;
+while (number >= 9)
+{
+_putchar(number + '0');
+_putchar('\n');
+number++;
+}
