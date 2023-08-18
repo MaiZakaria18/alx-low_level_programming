41b8474400d401b2e634900ae97f4e3ed604df76
Mai <mai.zakaria.18@gmail.com>
2023-08-18 13:29:45 -0700

    c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_diagonal - function that print diagonal
+ *
+ * @n: number of diagonal printed
+ */
+
+void print_diagonal(int n)
+{
+int x, z;
+if (n <= 0)
+{
+_putchar('\n');
+return;
+}
+for (int x = 0; x < n; x++)
+{
+for (int z = 0; z < i; z++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
