e40aceb0fdb6aad184f723b3adc6b84869397f93
Mai <mai.zakaria.18@gmail.com>
2023-08-18 13:33:58 -0700

    c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,7 +7,6 @@
 
 void print_diagonal(int n)
 {
-int x, z;
 if (n <= 0)
 {
 _putchar('\n');
@@ -15,7 +14,7 @@ return;
 }
 for (int x = 0; x < n; x++)
 {
-for (int z = 0; z < i; z++)
+for (int z = 0; z < x; z++)
 {
 _putchar(' ');
 }
