06d4231e990298de309fd9012cb4b0f4b53f2ee1
Mai <mai.zakaria.18@gmail.com>
2023-08-18 12:48:18 -0700

    c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,11 +10,13 @@ void print_line(int n)
 if (n <= 0)
 {
 _putchar('\n');
-return;
 }
-for (int i = 0; i < n; i++)
+else
+{
+for (int i = 0; i <= n; i++)
 {
 _putchar('_');
 }
 _putchar('\n');
+}
 }
