a106e1948a03952a5f6340e9a69ef0026a5c0dbd
Mai <mai.zakaria.18@gmail.com>
2023-08-20 04:44:37 -0700

    c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -17,12 +17,13 @@ else
 {
 int x;
 int y;
-for (x = 0; x <= size; x++;)
+for (x = 0; x <= size; x++)
 {
-for (y = 0; y <= size; y++;)
+for (y = 0; y <= size; y++)
 {
 _putchar(35);
 _putchar('\n');
+}
 }
 }
 }
