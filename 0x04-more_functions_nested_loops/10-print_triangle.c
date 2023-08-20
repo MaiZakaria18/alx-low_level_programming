802deec060565d3d37d65524379c76ed0797b709
Mai <mai.zakaria.18@gmail.com>
2023-08-20 07:08:03 -0700

    c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ *print_triangle -  function that prints a triangle
+ Descriptin -  function that prints a triangle
+ *@size: size of the triangle
+ *
+ */
+void print_triangle(int size)
+{
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int x;
+int y;
+for (x = 1; x <= size; x++)
+{
+for (y = 0; y < size - x; y++)
+{
+_putchar(' ');
+}
+int z;
+for (z = 0; z < x; z++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
