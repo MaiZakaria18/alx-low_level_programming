3f79c9cd15a37acf6736e47f09dd0649d874d4d6
Mai <mai.zakaria.18@gmail.com>
2023-08-20 06:09:41 -0700

    c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  * main - Entry point of the program
- *
  * Return: 0 on success
  */
 int main(void)
 {
 int num = 1;
+
 while ( num <= 100)
 {
 if (num % 3 == 0 && num % 5 == 0)
@@ -23,18 +23,10 @@ printf("Buzz");
 }
 else
 {
-if (num != 100)
-{
 printf("%d", num);
+}
 putchar(' ');
-}
-else
-{
-printf("%d", num);
-putchar('\n');
-}
 num++;
-}
 }
 return (0);
 }
