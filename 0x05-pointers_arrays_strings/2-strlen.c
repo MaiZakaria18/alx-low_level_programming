d56b765fd3763f49a3af0ad966fffa5ca8b1f697
Mai <mai.zakaria.18@gmail.com>
2023-08-21 13:28:39 -0700

    c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,7 +3,7 @@
  *  _strlen - Write a function that returns the length of a string.
  *  Description - Write a function that returns the length of a string.
  *  @s: charachter
- * Return: length of string 
+ * Return: length of string
  */
 int _strlen(char *s)
 {
