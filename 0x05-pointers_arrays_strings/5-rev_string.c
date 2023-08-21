29a21266cf0cf18071c99f429b72051fb25d5f82
Mai <mai.zakaria.18@gmail.com>
2023-08-21 08:02:41 -0700

    c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,18 +9,18 @@ void rev_string(char *s)
 int length;
 for (length = 0; s[length] != '\0'; length++)
 {
+}
+
 int normal_str;
 for (normal_str = 0; s[length] != '\0'; normal_str++)
 {
 _putchar(s[normal_str]);
 }
-}
-{
+
 int rev_str;
 for (rev_str = length - 1; rev_str >= 0; rev_str++)
 {
 _putchar(s[rev_str]);
-}
 }
 _putchar('\n');
 }
