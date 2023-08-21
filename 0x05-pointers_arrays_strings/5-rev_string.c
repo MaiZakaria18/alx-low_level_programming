ce24d636175a55dc903c3afe9454e4f9af95fa2d
Mai <mai.zakaria.18@gmail.com>
2023-08-21 08:27:45 -0700

    c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,7 +13,7 @@ char temp;
 for (length = 0; s[length] != '\0'; length++)
 {
 }
-for (str = 0, rev_str = length - 1; rev_str >= 0; rev_str--)
+for (str = 0, rev_str = length - 1; rev_str > str; str++,rev_str--)
 {
 temp = s[str];
 s[str] = s[rev_str];
