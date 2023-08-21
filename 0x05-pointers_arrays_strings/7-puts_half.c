f6f8b3773678132e7d38283f7c94d564d330852b
Mai <mai.zakaria.18@gmail.com>
2023-08-21 08:56:20 -0700

    c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,8 +7,9 @@
 void puts_half(char *str)
 {
 int length;
-for (length = 0; str[length] != '\0'; length++)
 int n;
+for (length = 0; str[length] != '\0'; length++)
+
 for (n = (length - 1) / 2; st[length] != '\0'; length++)
 {
 _putchar(str[length]);
