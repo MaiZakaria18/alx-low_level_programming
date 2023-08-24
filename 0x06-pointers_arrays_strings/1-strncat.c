4f2ba8feafd4fac58551ca963947a9092b879e86
Mai <mai.zakaria.18@gmail.com>
2023-08-24 01:10:02 -0700

    c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * _strncat - a function that concatenates two strings.
- * description - a function that concatenates two strings.
+ * _strncat - a function that concatenates two strings
+ * description - a function concatenates two strings
  * @src: string
  * @dest: string
  * @n: bytes from src
