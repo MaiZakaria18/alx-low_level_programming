f8e700cc00f1f33ad6fff4397ec94097f0db74c5
Mai <mai.zakaria.18@gmail.com>
2023-08-24 01:11:26 -0700

    c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,7 +18,7 @@ while (dest[length_d] != '\0')
 length_d++;
 }
 length_s = 0;
-while (src[length_s] != '\0' && n > 0)
+while (src[length_s] != '\0' && n > length_s)
 {
 dest[length_d++] = src[length_s];
 length_s++;
