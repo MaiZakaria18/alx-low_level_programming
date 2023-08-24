a47bf940219fbb7feee6b7b0b79bf061035146b8
Mai <mai.zakaria.18@gmail.com>
2023-08-24 01:42:19 -0700

    c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,13 +10,14 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int length;
-for (length = 0; n < length && src[length] != '\0'; length++)
+for (length = 0; n > length && src[length] != '\0'; length++)
 {
 dest[length] = src[length];
 }
-for (length = 0; length < n; length++)
+while (length < n)
 {
 dest[length] = '\0';
+length++;
 }
 return (dest);
 }
