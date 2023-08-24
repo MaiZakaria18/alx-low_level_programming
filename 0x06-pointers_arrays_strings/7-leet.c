e84e14e5dea4fc5547be1d8472e289d6913c3c02
Mai <mai.zakaria.18@gmail.com>
2023-08-24 03:38:45 -0700

    c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ *leet - that encodes a string into 1337.
+ *@string: string
+ * Return: string.
+ */
+char *leet(char *string)
+{
+char *ptr = string;
+while (*ptr)
+{
+char x = *ptr;
+if ((x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z'))
+{
+if (x == 'a' || x == 'A')
+{
+*ptr = '4';
+}
+else if (x == 'e' || x == 'E')
+{
+*ptr = '3';
+}
+else if (x == 'o' || x == 'O')
+{
+*ptr = '0';
+}
+else if (x == 't' || x == 'T')
+{
+*ptr = '7';
+}
+else if (x == 'l' || x == 'L')
+{
+*ptr = '1';
+}
+ptr++;
+}
+return (string);
+}
+}
