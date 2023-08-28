65c5b8adf3d27a72ae1f53fca9e478ec02e5ec70
Mai <mai.zakaria.18@gmail.com>
2023-08-28 12:44:01 -0700

    c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,9 +11,9 @@ int x;
 int y;
 for (x = 0; haystack[x] != '\0'; x++)
 {
-for (y = 0; needle[y] == haystack[x]; y++)
-if (needle[y] != '\0')
-return (haystack);
+for (y = 0; needle[y] == haystack[x + y]; y++)
+if (needle[y + 1] == '\0')
+return (haystack + x);
 }
 return (0);
 }
