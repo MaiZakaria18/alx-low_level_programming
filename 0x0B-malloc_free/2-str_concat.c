7d19e5d03723e9fd54af0040d610e3611f0379ae
Mai <mai.zakaria.18@gmail.com>
2023-09-05 13:06:10 -0700

    c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,25 +10,19 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-int i;
-char *conc;
-int size1 = (s1 != NULL) ? strlen(s1) : 0;
-int size2 = (s2 != NULL) ? strlen(s2) : 0;
-conc = malloc((size1 + size2 + 1) * sizeof(char));
-if (conc == 0)
+size_t len_s1, len_s2;
+char *result;
+if (s1 == NULL) s1 = "";
+if (s2 == NULL) s2 = "";
+len_s1 = strlen(s1);
+len_s2 = strlen(s2);
+result = malloc((len_s1 + len_s2 + 1) * sizeof(char));
+
+if (result == NULL)
 {
 return (NULL);
 }
-for (i = 0; i < size1; i++)
-{
-if (s1 == NULL)
-s1 = " ";
-conc[i] = s1[i];
-for (i = 0; i < size2; i++)
-{
-conc[size1 + i] = s2[i];
-}
-}
-conc[size1 + size2] = '\0';
-return (conc);
+strcpy(result, s1);
+strcat(result, s2);
+return (result);
 }
