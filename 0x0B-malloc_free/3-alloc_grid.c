c903a2099ae502e58d7889aa5f388a283070bd37
Mai <mai.zakaria.18@gmail.com>
2023-09-05 08:13:41 -0700

    c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,29 +11,29 @@
 int **alloc_grid(int width, int height)
 {
 int i, j;
+int **func;
 if (width <= 0 || height <= 0)
 {
 return (NULL);
 }
-char **func;
 func = malloc(sizeof(*func) * height);
 if (func == NULL)
 {
 return (NULL);
 }
-for (int i = 0; i < height; i++)
+for (i = 0; i < height; i++)
 {
 func[i] = malloc(sizeof(**func) * width);
 if (func[i] == NULL)
 {
-for (int j = 0; j < i; j++)
+for (j = 0; j < i; j++)
 {
 free(func[j]);
 }
 free(func);
 return (NULL);
 }
-for (int j = 0; j < width; j++)
+for (j = 0; j < width; j++)
 {
 func[i][j] = 0;
 }
