348bfc6927be6a76292a6dd4f5823f48e7ea8f03
Mai <mai.zakaria.18@gmail.com>
2023-09-07 11:00:51 -0700

    c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,14 +12,14 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 unsigned int m_size;
 void *s;
-if (ptr == NULL)
-{
-return (malloc(new_size));
-}
 if (new_size == 0)
 {
 free(ptr);
 return (NULL);
+}
+if (ptr == NULL)
+{
+return (malloc(new_size));
 }
 m_size = (old_size < new_size) ? old_size : new_size;
 s = malloc(new_size);
