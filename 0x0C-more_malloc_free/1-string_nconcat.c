ede4bb436ddcab8d51c9d567aef7325d05960c05
Mai <mai.zakaria.18@gmail.com>
2023-09-07 07:27:37 -0700

    c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,8 +11,8 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *result;
-size_t l1 = strlen(s1);
-size_t l2 = strlen(s2);
+size_t l_1 = strlen(s1);
+size_t l_2 = strlen(s2);
 if (s1 == NULL)
 {
 s1 = "";
@@ -21,9 +21,9 @@ if (s2 == NULL)
 {
 s2 = "";
 }
-result = malloc(l1 + n + 1);
-if (n >= l2)
-n = l2;
+result = malloc(l_1 + n + 1);
+if (n >= l_2)
+n = l_2;
 if (result == NULL)
 return (NULL);
 strcpy(result, s1);
