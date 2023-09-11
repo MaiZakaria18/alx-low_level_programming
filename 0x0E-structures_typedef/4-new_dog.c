12dc2c15d93ec2e8825ed1df9ce648952422ff10
Mai <mai.zakaria.18@gmail.com>
2023-09-11 13:23:02 -0700

    c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,12 +16,12 @@ dog_t *nDog;
 nDog = (dog_t *)malloc(sizeof(dog_t));
 if (nDog == NULL)
 return (NULL);
-nDog->name = malloc(sizeof(char) * (strlen(name) + 1));
 if (nDog->name == NULL)
 {
 free(nDog->name);
 return (NULL);
 }
+nDog->name = malloc(sizeof(char) * (strlen(name) + 1));
 strcpy(nDog->name, name);
 nDog->age = age;
 nDog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
@@ -30,6 +30,7 @@ if (nDog->owner == NULL)
 free(nDog->owner);
 return (NULL);
 }
+nDog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
 strcpy(nDog->owner, owner);
 return (nDog);
 }
