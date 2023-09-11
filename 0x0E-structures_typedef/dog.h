b0d509c84a6e6a4051bd448f65273d3056830e93
Mai <mai.zakaria.18@gmail.com>
2023-09-11 03:33:55 -0700

    c

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - func
+ * @name: string
+ * @age: float
+ * @owner: string
+ * Description - func
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+}
+
+#endif /* DOG_H */
