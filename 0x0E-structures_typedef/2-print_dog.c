b12ade9cc75a7da1729a1bc640db34017555ccb2
Mai <mai.zakaria.18@gmail.com>
2023-09-11 04:19:03 -0700

    c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 
 /**
  * print_dog - func
@@ -11,7 +12,7 @@ void print_dog(struct dog *d)
 if (d)
 {
 printf("Name: %s\n", (d->name) ? (d->name) : "(nill)");
-printf("Age: %f\n", (d->age) ? (d->age) : "(nill)");
+printf("Age: %f\n", (d->age));
 printf("Owner: %s\n", (d->owner) ? (d->owner) : "(nill)");
 }
 }
