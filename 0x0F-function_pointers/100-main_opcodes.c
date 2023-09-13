721346184b17c840b4c4db97838e5ffb438a11a3
Mai <mai.zakaria.18@gmail.com>
2023-09-13 04:43:53 -0700

    c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - func
+ * @argc: int
+ * @argv: pointer
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+char *x = (char *)main;
+int b;
+if (argc != 2)
+printf("Error\n"), exit(1);
+b = atoi(argv[1]);
+if (b < 0)
+printf("Error\n"), exit(2);
+while (b--)
+printf("%02hhx%s", *x++, b ? " ": "\n");
+return (0);
+}
