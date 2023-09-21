df182c2504378a941e9c433a037afd119ef8ffed
Mai <mai.zakaria.18@gmail.com>
2023-09-21 06:12:16 -0700

    lists

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - function that returns the number of elements
+ * @list: list
+ * Return: value
+ */
+size_t list_len(const list_t *h)
+{
+size_t x = 0;
+while (h)
+{
+h = h->next;
+x++;
+}
+return (x);
+}
