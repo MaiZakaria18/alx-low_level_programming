15afe879350932cdc4cca151012b4a407b5dca90
Mai <mai.zakaria.18@gmail.com>
2023-09-29 00:35:42 -0700

    bit_manipulation

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * flip_bits - function
+ * @n: pointer
+ * @m: int
+ * Return: result
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+int x, c = 0;
+unsigned long int y;
+unsigned long int z = n ^ m;
+for (x = 63; x >= 0; x--)
+{
+y = z >> x;
+if (y & 1)
+c++
+}
+return (c);
+}
