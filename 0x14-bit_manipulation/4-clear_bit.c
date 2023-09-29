df0cbb7b2118e3376a0381c16c7d4adf2536d2e5
Mai <mai.zakaria.18@gmail.com>
2023-09-29 00:24:23 -0700

    bit_manipulation

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * clear_bit - function
+ * @n: pointer
+ * @index: int
+ * Return: result
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+if (index > 63)
+return (-1);
+*n = ((1UL << index) | *n);
+return (1);
+}
