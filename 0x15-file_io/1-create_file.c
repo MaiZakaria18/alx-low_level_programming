3bc1aa539483e3e52b0e42bdce684bf966838d93
Mai <mai.zakaria.18@gmail.com>
2023-10-02 07:10:04 -0700

    file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * create_file - function
+ * @filename: pointer
+ * @text_content: pointer
+ * Return: 1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+int file, w, l = 0;
+if (filename == NULL)
+return (-1);
+
+if (text_content != NULL)
+{
+	for (l = 0; text_content[l];)
+		l++;
+}
+file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+w = write(file, text_content, l);
+if (file == -1 || w == -1)
+	return (-1);
+close(file);
+return (1);
+}
