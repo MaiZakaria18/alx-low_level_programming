a92d237c91a17fcf9511c1022eef99c908119fa5
Mai <mai.zakaria.18@gmail.com>
2023-10-02 06:48:29 -0700

    file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * read_textfile - function
+ * @filename: pointer
+ * @letters: char
+ * Return: w
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+char *b;
+ssize_t file;
+ssize_t r;
+ssize_t w;
+
+file = open(filename, O_RONLY);
+if (file == -1)
+return (0);
+
+b = malloc(sizeof(char) * letters);
+r = read(file, b, letters);
+w = write(STDOUT_FILENO, b, r);
+
+free(b);
+close(file);
+return (w);
+}
