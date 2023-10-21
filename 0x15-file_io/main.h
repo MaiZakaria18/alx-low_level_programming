ce31cbaf544b6f5c606f0547a6acf4859ef838f8
Mai <mai.zakaria.18@gmail.com>
2023-10-20 19:37:41 -0700

    c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,7 +7,6 @@
 #include <stdio.h>
 #include <elf.h>
 
-int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
@@ -19,4 +18,5 @@ void print_osabi(unsigned char *e_ident);
 void print_abiversion(unsigned char *e_ident);
 void print_type(uint16_t e_type);
 void print_entry(uint64_t e_entry);
+
 #endif
