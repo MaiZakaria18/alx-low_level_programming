ce2cd2de13dad204ee10ca80e2bd2bfadeee2e81
Mai <mai.zakaria.18@gmail.com>
2023-10-20 19:57:01 -0700

    c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,22 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <elf.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
 
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+int _strlen(char *str);
 int append_text_to_file(const char *filename, char *text_content);
-void print_magic(unsigned char *e_ident);
-void print_class(unsigned char *e_ident);
-void print_data(unsigned char *e_ident);
-void print_version(unsigned char *e_ident);
-void print_osabi(unsigned char *e_ident);
-void print_abiversion(unsigned char *e_ident);
-void print_type(uint16_t e_type);
-void print_entry(uint64_t e_entry);
 
 #endif
