f13d2d16df64fbb1eedcda96126184af765829be
Mai <mai.zakaria.18@gmail.com>
2023-10-20 19:01:48 -0700

    file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,118 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdint.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <elf.h>
+#include "main.h"
 /**
- * print_elf_header - func
- * @header: h
+ *main - entry point
+ *@argc: argument count
+ *@argv: argument value
+ *Return: 0 to success
  */
-void print_elf_header(const Elf64_Ehdr *header)
+int main(int argc, char **argv)
 {
-printf("  Magic:   ");
-for (int i = 0; i < EI_NIDENT; i++)
-{
-printf("%02x ", header->e_ident[i]);
-}
-printf("\n");
-printf("  Class:                             ");
-switch (header->e_ident[EI_CLASS])
-{
-case ELFCLASS32:
-printf("ELF32\n");
-break;
-case ELFCLASS64:
-printf("ELF64\n");
-break;
-default:
-printf("Invalid\n");
-break;
-}
-printf("  Data:                              ");
-switch (header->e_ident[EI_DATA]) {
-case ELFDATA2LSB:
-printf("2's complement, little-endian\n");
-break;
-case ELFDATA2MSB:
-printf("2's complement, big-endian\n");
-break;
-default:
-printf("Invalid\n");
-break;
-}
-printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
-printf("  OS/ABI:                            ");
-switch (header->e_ident[EI_OSABI]) {
-case ELFOSABI_SYSV:
-printf("UNIX - System V\n");
-break;
-case ELFOSABI_HPUX:
-printf("HP-UX\n");
-break;
-case ELFOSABI_STANDALONE:
-printf("Standalone App\n");
-break;
-default:
-printf("Other\n");
-break;
-}
-printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
-printf("  Type:                              ");
-switch (header->e_type) {
-case ET_NONE:
-printf("NONE (None)\n");
-break;
-case ET_REL:
-printf("REL (Relocatable file)\n");
-break;
-case ET_EXEC:
-printf("EXEC (Executable file)\n");
-break;
-case ET_DYN:
-printf("DYN (Shared object file)\n");
-break;
-case ET_CORE:
-printf("CORE (Core file)\n");
-break;
-default:
-printf("Other\n");
-break;
-}
-printf("  Entry point address:               0x%lx\n", (unsigned long)header->e_entry);
-}
-/**
- * main - func
- * @argc: i
- * @argv: iny
- * Return :0
- */
-int main(int argc, char *argv[]) {
-if (argc != 2) {
-fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
-return 98;
-}
-int fd = open(argv[1], O_RDONLY);
-if (fd == -1) {
-perror("Failed to open the file");
-return 98;
-}
-Elf64_Ehdr header;
-if (read(fd, &header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr))
-{
-fprintf(stderr, "Error reading ELF header from file\n");
-close(fd);
-return 98;
-}
-if (memcmp(header.e_ident, ELFMAG, SELFMAG) != 0)
-{
-fprintf(stderr, "Not an ELF file\n");
-close(fd);
-return 98;
-}
-printf("ELF Header:\n");
-print_elf_header(&header);
-close(fd);
-return 0;
+	int fd;
+	Elf64_Ehdr ehdr;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+		return (98);
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		perror(argv[1]);
+		return (98);
+	}
+	if (read(fd, &ehdr, sizeof(ehdr)) != sizeof(ehdr))
+	{
+		fprintf(stderr, "%s is not an ELF file\n", argv[1]);
+		return (98);
+	}
+	if (ehdr.e_ident[EI_MAG0] != ELFMAG0 || ehdr.e_ident[EI_MAG1] != ELFMAG1
+	|| ehdr.e_ident[EI_MAG2] != ELFMAG2 || ehdr.e_ident[EI_MAG3] != ELFMAG3)
+	{
+		fprintf(stderr, "%s is not an ELF file\n", argv[1]);
+		return (98);
+	}
+	printf("ELF Header:\n");
+	print_magic(ehdr.e_ident);
+	print_class(ehdr.e_ident);
+	print_data(ehdr.e_ident);
+	print_version(ehdr.e_ident);
+	print_osabi(ehdr.e_ident);
+	print_abiversion(ehdr.e_ident);
+	print_type(ehdr.e_type);
+	print_entry(ehdr.e_entry);
+	close(fd);
+	return (0);
 }
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,70 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <errno.h>
-
+#include "main.h"
 #define BUFFER_SIZE 1024
-
 /**
- * main - func
- * @argc: int
- * @argv: char
- * Return: 0
+ *main - program
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: 1 on success 0 on failure
  */
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
-if (argc != 3) 
-{
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit(97);
-}
-char *file_from = argv[1];
-char *file_to = argv[2];
-int fd_from = open(file_from, O_RDONLY);
-if (fd_from == -1) 
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-exit(98);
-}
-int fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP);
-if (fd_to == -1) 
-{
-dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
-exit(99);
-}
-char buffer[BUFFER_SIZE];
-ssize_t bytes_read, bytes_written;
-while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0) 
-{
-bytes_written = write(fd_to, buffer, bytes_read);
-if (bytes_written == -1) 
-{
-dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
-exit(99);
-}
-if (bytes_written != bytes_read) 
-{
-dprintf(STDERR_FILENO, "Error: Incomplete write to file %s\n", file_to);
-exit(99);
-}
-}
-if (bytes_read == -1) 
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-exit(98);
-}
-if (close(fd_from) == -1) 
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-exit(100);
-}
-if (close(fd_to) == -1) 
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-exit(100);
-}
-return 0;
+	int fileFrom, fileTo;
+	char buff[BUFFER_SIZE];
+	ssize_t bytesRead, bytesWritten;
+
+	if (argc != 3)
+	{ dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97); }
+	fileFrom = open(argv[1], O_RDONLY);
+	if (fileFrom < 0)
+	{ dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98); }
+	fileTo = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fileTo < 0)
+	{ dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(fileFrom);
+		exit(99); }
+	while ((bytesRead = read(fileFrom, buff, BUFFER_SIZE)) > 0)
+	{ bytesWritten = write(fileTo, buff, bytesRead);
+		if (bytesWritten != bytesRead)
+		{ dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(fileFrom);
+			close(fileTo);
+			exit(99); } }
+	if (bytesRead < 0)
+	{ dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98); }
+	if (close(fileFrom) < 0)
+	{ dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileFrom);
+		exit(100); }
+	if (close(fileTo) < 0)
+	{ dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileTo);
+		exit(100); }
+	return (0);
 }
