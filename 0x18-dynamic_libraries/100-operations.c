44c9fb27344c3577a93abc2ab9f04dce03c2a0bc
Mai <mai.zakaria.18@gmail.com>
2023-11-13 03:48:09 -0800

    c

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+int divide(int a, int b) {
+    if (b != 0) {
+        return a / b;
+    } else {
+        return 0;
+    }
+}
+
+int modulus(int a, int b) {
+    if (b != 0) {
+        return a % b;
+    } else {
+        return 0;
+    }
+}
